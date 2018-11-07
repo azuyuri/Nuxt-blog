786e5e024bcacf4b31f96009a419996faf82fd8c
azuyuri <tamondojo@gmail.com>
2018-11-07 20:23:45 +0900

    feat:header追加

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,22 @@
 <template>
   <div>
-    <nuxt/>
+    <TheHeader />
+    <main class="main-content">
+      <nuxt/>
+    </main>
   </div>
 </template>
+
+<script>
+import TheHeader from '@/components/TheHeader/TheHeader'
+
+export default {
+  components: {
+    TheHeader
+  }
+}
+</script>
+
 
 <style>
 * {
@@ -11,5 +25,9 @@
 
 body {
   font-family: "Lato", sans-serif;
+}
+
+.main-content {
+  margin-top: 4.5rem;
 }
 </style>
