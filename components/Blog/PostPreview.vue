de39412b434bd894ee7a50c811a20d5eb900fc4a
azuyuri <tamondojo@gmail.com>
2018-11-08 18:03:44 +0900

    chore:PostPreview components 追加

---
+++ components/Blog/PostPreview.vue
@@ -0,0 +1,16 @@
+<template>
+  <nuxt-link
+      v-for="post in posts"
+      :key="post.id"
+      to="post.id">
+      <article class="post-preview">
+        <div
+          :style="{backgroundImage: post.thumbnailUrl}"
+          class="post-preview-thumbnail"/>
+        <div class="post-preview-content">
+          <h1>{{ post.title }}</h1>
+          <p>{{ post.previewText }}</p>
+        </div>
+      </article>
+    </nuxt-link>
+</template>
